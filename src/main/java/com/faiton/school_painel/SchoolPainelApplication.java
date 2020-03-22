33d65456e322f7c5f857680cdd90affd85988dfa
Franklin Oliveira da Veiga <faiton@live.com>
2020-03-22 15:47:20 -0300

    First files

---
+++ src/main/java/com/faiton/school_painel/SchoolPainelApplication.java
@@ -0,0 +1,13 @@
+package com.faiton.school_painel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolPainelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolPainelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/faiton/school_painel/SchoolPainelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faiton.school_painel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolPainelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
