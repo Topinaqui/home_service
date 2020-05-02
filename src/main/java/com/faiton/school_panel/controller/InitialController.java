021ce3f6acaa4b60c94fd51166b3645a662a04c3
Franklin Oliveira da Veiga <faiton@live.com>
2020-05-01 22:31:45 -0300

    Acrescentando cors config ao InitController

--- src/main/java/com/faiton/school_panel/controller/InitialController.java
+++ src/main/java/com/faiton/school_panel/controller/InitialController.java
@@ -2,6 +2,7 @@ package com.faiton.school_panel.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.faiton.school_panel.model.News;
 import com.faiton.school_panel.model.School;
 import com.faiton.school_panel.repository.NewsRepository;
 
+@CrossOrigin( origins = "*", allowedHeaders = "*" )
 @RestController
 public class InitialController {
 
